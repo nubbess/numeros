cfddb829c8158240043f4ad7875a82d6dcc3e54b
Julián <julianms999@gmail.com>
2022-12-02 20:25:18 -0300

    Create tarea números openbootcamp.js

---
+++ tarea números openbootcamp.js
@@ -0,0 +1,30 @@
+let estaturaCm = 186;
+
+let estaturaMt = 1.86;
+
+let pesoKg = 75.5;
+
+let estaturaRed = estaturaMt.toFixed(1);
+
+let redondeoKg = Math.floor(pesoKg);
+
+console.log(estaturaRed);
+
+console.log(redondeoKg);
+
+// What d'y mean? está confusa la consigna del último punto, pero aquí van varias cosas que entendí:
+
+/*Una variable que contenga si "el máximo valor que se puede obtener en Javascript + 1" 
+es igual al máximo valor que se puede obtener en Javascript */
+
+let maxval = Number.MAX_VALUE;
+
+let esIgual = maxval + 1 === maxval;
+
+console.log(esIgual);
+
+// o, agregando, porque la consigna dice si...
+
+if (maxval + 1 === maxval) {
+  console.log("es igual")
+}
